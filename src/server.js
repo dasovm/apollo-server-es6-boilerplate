50be8d3cb16736bb35db2a75451f81cb94cd76cc
David Söderberg <dasovm@gmail.com>
2018-06-23 00:48:14 +0200

    Added graphql config

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 import { ApolloServer, gql } from 'apollo-server';
 import glue from 'schemaglue';
+import fs from 'fs';
 
 const { schema, resolver } = glue('./src/graphql');
+
+fs.writeFile('./src/generated.graphql', schema, 'utf8', err => {
+  if (err) return console.error(err);
+  console.info('📝 Saved generated.graphql');
+  return true;
+});
 
 const server = new ApolloServer({
   typeDefs: gql(schema),
